506d6c4978da89b6c0b3b5db011d447b099ff7fa
Maya-Murali-88 <smiyamaya@gmail.com>
2025-01-19 23:32:48 +1000

    second commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import { CodeViewer } from "@/components/code-viewer";
-import { PresetSave } from "@/components/preset-save";
-import { PresetSelector } from "@/components/preset-selector";
-import { PresetShare } from "@/components/preset-share";
 
 export function Header() {
   return (
