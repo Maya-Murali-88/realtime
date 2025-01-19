aaceea5587553a723f89c5a939689368c3c3a9b6
Maya-Murali-88 <smiyamaya@gmail.com>
2025-01-20 00:04:19 +1000

    Update instructions

--- src/components/instructions.tsx
+++ src/components/instructions.tsx
@@ -3,9 +3,25 @@
 import { useState } from "react";
 import { usePlaygroundState } from "@/hooks/use-playground-state";
 
-export function Instructions() {
-  const [isFocused, setIsFocused] = useState<boolean>(false);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const { pgState } = usePlaygroundState();
-             
-}
+export function Instructions(): JSX.Element {
+    const [isFocused, setIsFocused] = useState<boolean>(false);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const { pgState } = usePlaygroundState();
+  
+    return (
+      <div
+        className={`flex flex-1 flex-col w-full gap-[4px] border p-4 rounded-lg ${
+          isFocused ? "ring-1" : "ring-0"
+        } h-[200px] overflow-y-auto`}
+      >
+        <div className="flex justify-between items-center mb-2">
+          <div className="flex items-center">
+            <div className="text-xs font-semibold uppercase mr-1 tracking-widest">
+              INSTRUCTIONS
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
